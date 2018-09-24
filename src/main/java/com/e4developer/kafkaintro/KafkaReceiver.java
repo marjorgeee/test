978b25e89cdc1f80c63e3efb0ec4afc3836c8f25
Salivio <eric.jason.salivio@accenture.com>
2018-09-24 13:16:53 +0800

    dev final item

--- src/main/java/com/e4developer/kafkaintro/KafkaReceiver.java
+++ src/main/java/com/e4developer/kafkaintro/KafkaReceiver.java
@@ -9,7 +9,7 @@ public class KafkaReceiver {
 
     @KafkaListener(topics = "topic1")
     public void receiveTopic1(ConsumerRecord<?, ?> consumerRecord) {
-        System.out.println("Receiver on topic1: "+consumerRecord.toString());
+        System.out.println("Receiver on topictest1: "+consumerRecord.toString());
     }
 
     @KafkaListener(topics = "topic2")
